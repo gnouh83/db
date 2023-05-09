f4142ac33c98235c6f57430e48893a92bbdbd097
HuongNV <huongnv@telsoft.com.vn>
2023-05-09 14:21:43 +0700

    - Updated

--- dsp.sql
+++ dsp.sql
@@ -1,18 +1,21 @@
 SELECT *
-FROM dsp_company;
+FROM dsp_company
+WHERE type = 1
+ORDER BY com_name;
 SELECT *
 FROM dsp_sms_command
 ORDER BY cmd_id;
 
 SELECT *
 FROM dsp_sys_log
-WHERE exec_datetime >= trunc(sysdate) and TRANS_ID='vmedia_api_com_1670474904633'
+WHERE exec_datetime >= TRUNC(SYSDATE)
+  AND trans_id = 'vmedia_api_com_1670474904633'
 ORDER BY log_id;
 
 SELECT *
 FROM dsp_sys_log
 WHERE exec_datetime >= TO_DATE('15/12/2022', 'dd/mm/yyyy')
-  AND ISDN = '906328167'
+  AND isdn = '906328167'
 ORDER BY log_id;
 
 
@@ -112,7 +115,7 @@ SELECT *
 FROM temp_comp_20220906;
 
 SELECT *
-FROM DSP_COMPANY;
+FROM dsp_company;
 
 SELECT bus_code, email, public_key, get_serial_prefix(com_id) serial_prefix, user_id
 FROM dsp_company
@@ -125,41 +128,41 @@ FROM dsp_company
 WHERE com_id = 881;
 
 SELECT *
-FROM DSP_SMS_COMMAND
-WHERE CMD_CODE = 'LOYALTY';
+FROM dsp_sms_command
+WHERE cmd_code = 'LOYALTY';
 
 SELECT *
-FROM DSP_MT_HISTORY
-WHERE ISDN = '84937122801'
-  AND SENT_TIME >= TO_DATE('09/10/2022', 'dd/mm/yyyy');
+FROM dsp_mt_history
+WHERE isdn = '84937122801'
+  AND sent_time >= TO_DATE('09/10/2022', 'dd/mm/yyyy');
 
 
 SELECT *
-FROM DSP_SYS_LOG
-ORDER BY LOG_ID DESC;
+FROM dsp_sys_log
+ORDER BY log_id DESC;
 
 SELECT *
-FROM DSP_MO_QUEUE;
+FROM dsp_mo_queue;
 
 SELECT *
-FROM DSP_MO_HISTORY
-WHERE ISDN = '937122801'
-  AND RECEIVED_TIME >= TO_DATE('09/10/2022', 'dd/mm/yyyy')
-ORDER BY REQUEST_ID DESC;
+FROM dsp_mo_history
+WHERE isdn = '937122801'
+  AND received_time >= TO_DATE('09/10/2022', 'dd/mm/yyyy')
+ORDER BY request_id DESC;
 --987433123
 
 
 /*
 insert into  DSP_MO_QUEUE values (1,'936009977',null,'TRACUU 000000000004328',sysdate,'9999','3');
 commit;*/
-INSERT INTO DSP_OWNER.DSP_SMS_COMMAND (CMD_ID, CMD_CODE, CMD_TYPE, CMD_MSG_CONTENT, CMD_PARAM_COUNT, DESCRIPTION,
-                                       CMD_REGEX, STATUS)
+INSERT INTO dsp_owner.dsp_sms_command (cmd_id, cmd_code, cmd_type, cmd_msg_content, cmd_param_count, description,
+                                       cmd_regex, status)
 VALUES (71, 'DK_EDU_F2', 'O',
         'Đăng ký không thành công dịch vụ, thuê bao đã có gói cước. Chi tiết liên hệ 9090. Xin cảm ơn!', 0,
         'Đăng ký không thành công dịch vụ, thuê bao đã có gói cước. Chi tiết liên hệ 9090. Xin cảm ơn!', NULL, '1');
 
 
-INSERT INTO DSP_MO_QUEUE
+INSERT INTO dsp_mo_queue
 VALUES (1, '906045666', '', 'EDU MA3309682438120', SYSDATE, '9999', '3');
 COMMIT;
 
@@ -184,17 +187,17 @@ WITH rpt_dc_order_summary AS (SELECT order_code,
                               GROUP BY order_code, cre_dat, order_id, addon, profile_code, total
                               ORDER BY order_id, profile_code)
 SELECT *
-FROM (SELECT dt.com_name                                 DOANH_NGHIEP,
-             TO_CHAR(t.transaction_id)                   MA_ORDER,
+FROM (SELECT dt.com_name                                 doanh_nghiep,
+             TO_CHAR(t.transaction_id)                   ma_order,
              s.service_name,
              o.profile_code,
-             p.price                                     PRICE,
+             p.price                                     price,
              o.used_in_period,
-             p.price * o.used_in_period                  TOT_USED,
+             p.price * o.used_in_period                  tot_used,
              o.expired_in_period,
-             p.price * o.expired_in_period               TOT_EXPIRED,
-             p.price * (o.not_yet - o.expired_in_period) TOT_REMAIN,
-             p.price * (o.total - o.used)                TOT_START,
+             p.price * o.expired_in_period               tot_expired,
+             p.price * (o.not_yet - o.expired_in_period) tot_remain,
+             p.price * (o.total - o.used)                tot_start,
              o.not_yet
       FROM dsp_transaction t,
            dsp_company_leveled dt,
@@ -212,13 +215,13 @@ FROM (SELECT dt.com_name                                 DOANH_NGHIEP,
                            FROM rpt_dc_order_summary
                            WHERE (expired_in_period > 0 OR used_in_period > 0))
       UNION ALL
-      SELECT dt.com_name        DOANH_NGHIEP,
-             'API'              MA_ORDER,
-             s.service_name     DICH_VU,
-             h.profile          PROFILE,
-             sp.price           DON_GIA,
-             SUM(amount) / 1024 SAN_LUONG,
-             SUM(h.req_cost)    THANH_TIEN,
+      SELECT dt.com_name        doanh_nghiep,
+             'API'              ma_order,
+             s.service_name     dich_vu,
+             h.profile          profile,
+             sp.price           don_gia,
+             SUM(amount) / 1024 san_luong,
+             SUM(h.req_cost)    thanh_tien,
              0,
              0,
              0,
@@ -242,7 +245,7 @@ FROM (SELECT dt.com_name                                 DOANH_NGHIEP,
         AND h.request_time < TO_DATE('10/10/2022', 'dd/mm/yyyy') + 1
         AND dt.top_id = 542
       GROUP BY dt.com_name, s.service_name, h.profile, sp.price)
-ORDER BY DOANH_NGHIEP, MA_ORDER, profile_code, PRICE;
+ORDER BY doanh_nghiep, ma_order, profile_code, price;
 
 SELECT *
 FROM dsp_company_leveled;
@@ -260,37 +263,37 @@ SELECT order_code,
 FROM rpt_dc_order_summary_daily
 WHERE sum_dat >= TO_DATE('01/01/2022', 'dd/mm/yyyy')
   AND sum_dat < TO_DATE('10/10/2022', 'dd/mm/yyyy') + 1
-  AND ORDER_CODE = 'dO5SVIExyM93asUvzmJNvQxtPFc='
+  AND order_code = 'dO5SVIExyM93asUvzmJNvQxtPFc='
 GROUP BY order_code, cre_dat, order_id, addon, profile_code, total
 ORDER BY order_id, profile_code;
 
 SELECT *
 FROM dsp_transaction
-WHERE TRANSACTION_ID = 922;
+WHERE transaction_id = 922;
 
 
 SELECT *
-FROM RPT_DC_ORDER_SUMMARY_DAILY
-WHERE ORDER_CODE = 'iLYWi+y7rU+VzyWz9db9QTzWvDM='
-ORDER BY SUM_DAT;
+FROM rpt_dc_order_summary_daily
+WHERE order_code = 'iLYWi+y7rU+VzyWz9db9QTzWvDM='
+ORDER BY sum_dat;
 --SUM_DAT,ORDER_ID,ORDER_CODE,CRE_DAT,ADDON,PROFILE_CODE,TOTAL,USED_IN_PERIOD,USED,NOT_YET,EXPIRED_IN_PERIOD
 
 SELECT *
-FROM RPT_DC_ORDER_SUMMARY_DAILY
-WHERE TOTAL = EXPIRED_IN_PERIOD;
+FROM rpt_dc_order_summary_daily
+WHERE total = expired_in_period;
 
-UPDATE RPT_DC_ORDER_SUMMARY_DAILY
-SET EXPIRED_IN_PERIOD=NOT_YET
-WHERE TOTAL = EXPIRED_IN_PERIOD;
+UPDATE rpt_dc_order_summary_daily
+SET expired_in_period=not_yet
+WHERE total = expired_in_period;
 COMMIT;
 
 SELECT *
-FROM API;
+FROM api;
 
 
 
 SELECT o.com_id,
-       o.ORDER_ID,
+       o.order_id,
        CASE
            WHEN o.order_time >= TO_DATE('09/04/2022', 'dd/mm/yyyy') THEN NVL(o.contract_value, 0)
            ELSE NVL(ol.remain_value, 0)
@@ -310,7 +313,7 @@ SELECT o.com_id,
            END               remain_value,
        TO_DATE('09/04/2022', 'dd/mm/yyyy')
 FROM dsp_order o,
-     (SELECT * FROM TMP_RPT_ORDER_SUMMARY_DAILY WHERE sum_date = TO_DATE('09/04/2022', 'dd/mm/yyyy') - 1) ol,
+     (SELECT * FROM tmp_rpt_order_summary_daily WHERE sum_date = TO_DATE('09/04/2022', 'dd/mm/yyyy') - 1) ol,
      (SELECT order_id, SUM(NVL(ot.amount, 0)) amount_used
       FROM dsp_order_transaction ot,
            dsp_transaction t
@@ -318,22 +321,22 @@ FROM dsp_order o,
         AND t.status = 6
         AND ot.issue_time >= TO_DATE('09/04/2022', 'dd/mm/yyyy')
         AND ot.issue_time < TO_DATE('10/04/2022', 'dd/mm/yyyy')
-      GROUP BY ot.ORDER_ID) u
+      GROUP BY ot.order_id) u
 WHERE o.order_id = ol.order_id(+)
-  AND o.ORDER_ID = u.ORDER_ID(+)
+  AND o.order_id = u.order_id(+)
   AND TRUNC(o.order_time) < TO_DATE('10/04/2022', 'dd/mm/yyyy')
   AND o.expire_time >= TO_DATE('09/04/2022', 'dd/mm/yyyy');
 
 
-CREATE TABLE RPT_ORDER_SUMMARY_DAILY_BAK AS
+CREATE TABLE rpt_order_summary_daily_bak AS
 SELECT *
-FROM RPT_ORDER_SUMMARY_DAILY
-ORDER BY SUM_DATE;
+FROM rpt_order_summary_daily
+ORDER BY sum_date;
 
 
 SELECT *
-FROM RPT_ORDER_SUMMARY_DAILY
-WHERE REMAIN_VALUE < 0;
+FROM rpt_order_summary_daily
+WHERE remain_value < 0;
 
 DECLARE
     dfrom date;
@@ -346,142 +349,142 @@ BEGIN
 
     WHILE day <= dtill
         LOOP
-            DBMS_OUTPUT.PUT_LINE(day);
+            dbms_output.put_line(day);
             summary_order_daily(day);
             day := day + 1;
         END LOOP;
 END;
 /
-begin
-    SUMMARY_ORDER_MONTHLY(to_date('01/12/2021','dd/mm/yyyy'));
-    SUMMARY_ORDER_MONTHLY(to_date('01/01/2022','dd/mm/yyyy'));
-    SUMMARY_ORDER_MONTHLY(to_date('01/02/2022','dd/mm/yyyy'));
-    SUMMARY_ORDER_MONTHLY(to_date('01/03/2022','dd/mm/yyyy'));
-    DBMS_OUTPUT.PUT_LINE('Done!');
-end;
+BEGIN
+    summary_order_monthly(TO_DATE('01/12/2021', 'dd/mm/yyyy'));
+    summary_order_monthly(TO_DATE('01/01/2022', 'dd/mm/yyyy'));
+    summary_order_monthly(TO_DATE('01/02/2022', 'dd/mm/yyyy'));
+    summary_order_monthly(TO_DATE('01/03/2022', 'dd/mm/yyyy'));
+    dbms_output.put_line('Done!');
+END;
 
 
 SELECT *
-FROM RPT_ORDER_SUMMARY_DAILY
-WHERE REMAIN_VALUE < 0;
+FROM rpt_order_summary_daily
+WHERE remain_value < 0;
 SELECT *
-FROM TMP_2_RPT_ORDER_SUMMARY_DAILY
-WHERE REMAIN_VALUE < 0
-ORDER BY SUM_DATE;
+FROM tmp_2_rpt_order_summary_daily
+WHERE remain_value < 0
+ORDER BY sum_date;
 
 SELECT *
-FROM DSP_ORDER
-WHERE ORDER_ID = 745
-ORDER BY ORDER_TIME;
+FROM dsp_order
+WHERE order_id = 745
+ORDER BY order_time;
 
 SELECT *
-FROM DSP_ORDER_TRANSACTION
-WHERE TRANSACTION_ID = 1520;
+FROM dsp_order_transaction
+WHERE transaction_id = 1520;
 
 SELECT *
-FROM DSP_TRANSACTION
-ORDER BY REQUEST_TIME DESC;
+FROM dsp_transaction
+ORDER BY request_time DESC;
 SELECT *
-FROM DSP_TRANSACTION
-WHERE TRANSACTION_ID = 1520;
+FROM dsp_transaction
+WHERE transaction_id = 1520;
 SELECT *
-FROM DSP_DD_DETAIL
-WHERE TRANSACTION_ID = 1520;
+FROM dsp_dd_detail
+WHERE transaction_id = 1520;
 SELECT *
-FROM DSP_DD_HISTORY
-WHERE TRANSACTION_ID = '1520';
+FROM dsp_dd_history
+WHERE transaction_id = '1520';
 
 SELECT 87110 - 6445
-FROM DUAL;
+FROM dual;
 
 
 SELECT *
-FROM DSP_LOCK
+FROM dsp_lock
 WHERE type = '1'
-  AND LOCKED_OBJECT = 'ct2_mpl_api';
+  AND locked_object = 'ct2_mpl_api';
 
-DELETE DSP_LOCK
-WHERE LOCKED_OBJECT = 'ct2_mpl_api'
-  AND ISSUE_DATE = TRUNC(SYSDATE);
+DELETE dsp_lock
+WHERE locked_object = 'ct2_mpl_api'
+  AND issue_date = TRUNC(SYSDATE);
 COMMIT;
 
 SELECT *
-FROM DSP_SYS_LOG
-WHERE EXEC_DATETIME >= TRUNC(SYSDATE)
-  AND TRANS_ID LIKE 'ct2_mpl_api%'
-  AND REQUEST LIKE 'UseDCReqObj%'
-  AND STATUS = '0';
+FROM dsp_sys_log
+WHERE exec_datetime >= TRUNC(SYSDATE)
+  AND trans_id LIKE 'ct2_mpl_api%'
+  AND request LIKE 'UseDCReqObj%'
+  AND status = '0';
 --and REQUEST like '%020000007108057%';
 
 SELECT *
-FROM DSP_TRANSACTION
-WHERE TRANSACTION_ID = 2449;
+FROM dsp_transaction
+WHERE transaction_id = 2449;
 
-SELECT TABLE_NAME,
-       COLUMN_NAME,
-       DATA_TYPE || '(' || DECODE(DATA_TYPE, 'NUMBER', DATA_PRECISION, DATA_LENGTH) || ')' DATA_LENGTH,
-       DECODE(NULLABLE, 'N', 'Y', 'N')                                                     mandatory
-FROM USER_TAB_COLUMNS
-WHERE TABLE_NAME LIKE 'DIP_REQUEST_HIST%';
-
-SELECT *
-FROM USER_TAB_COLUMNS
-WHERE TABLE_NAME = '2.7.2.21	DIP_REQUEST_HIST_QUEUE';
-SELECT *
-FROM USER_CONSTRAINTS a,
-     USER_CONS_COLUMNS b
-WHERE a.TABLE_NAME = 'DSP_TRANSACTION'
-  AND a.CONSTRAINT_TYPE <> 'C'
-  AND a.CONSTRAINT_NAME = b.CONSTRAINT_NAME;
+SELECT table_name,
+       column_name,
+       data_type || '(' || DECODE(data_type, 'NUMBER', data_precision, data_length) || ')' data_length,
+       DECODE(nullable, 'N', 'Y', 'N')                                                     mandatory
+FROM user_tab_columns
+WHERE table_name LIKE 'DIP_REQUEST_HIST%';
 
 SELECT *
-FROM DSP_SYS_LOG;
+FROM user_tab_columns
+WHERE table_name = '2.7.2.21	DIP_REQUEST_HIST_QUEUE';
 SELECT *
-FROM USER_TAB_COLUMNS;
-SELECT *
-FROM DSP_COM_ORDER_POL;
+FROM user_constraints a,
+     user_cons_columns b
+WHERE a.table_name = 'DSP_TRANSACTION'
+  AND a.constraint_type <> 'C'
+  AND a.constraint_name = b.constraint_name;
 
 SELECT *
-FROM DSP_TRANSACTION
-WHERE TRANSACTION_ID = 2480;
+FROM dsp_sys_log;
+SELECT *
+FROM user_tab_columns;
+SELECT *
+FROM dsp_com_order_pol;
 
 SELECT *
-FROM DSP_MO_QUEUE_20220627;
-SELECT *
-FROM USER_TABLES
-ORDER BY TABLE_NAME;
+FROM dsp_transaction
+WHERE transaction_id = 2480;
 
 SELECT *
-FROM DSP_MO_HISTORY
-WHERE RECEIVED_TIME >= TRUNC(SYSDATE - 3)
-  AND CONTENT = 'DK DC N49724941953902';
+FROM dsp_mo_queue_20220627;
 SELECT *
-FROM DSP_MT_HISTORY
-WHERE ISDN = '84765519351'
-  AND SENT_TIME >= TRUNC(SYSDATE - 3);
+FROM user_tables
+ORDER BY table_name;
 
 SELECT *
-FROM DSP_SYS_LOG
-WHERE REQUEST LIKE '%%';
+FROM dsp_mo_history
+WHERE received_time >= TRUNC(SYSDATE - 3)
+  AND content = 'DK DC N49724941953902';
+SELECT *
+FROM dsp_mt_history
+WHERE isdn = '84765519351'
+  AND sent_time >= TRUNC(SYSDATE - 3);
+
+SELECT *
+FROM dsp_sys_log
+WHERE request LIKE '%%';
 
 --UseDCResObj{transaction_id='10207', code='0', description='null', serial='230000000004746', transaction_id='10207', dat_amt=0, dat_day=180, order_code='mAUBRea2e35g390EYAclsmNffzU=', reseller='100000000241', profile_code='6MA30'}
 
 
 SELECT *
-FROM DSP_ORDER_TRANSACTION;
+FROM dsp_order_transaction;
 SELECT *
 FROM dip_sub_service
 WHERE isdn = '918861983'
-ORDER BY END_TIME DESC;
+ORDER BY end_time DESC;
 --931040211;
 
 SELECT *
-FROM DSP_MO_QUEUE;
+FROM dsp_mo_queue;
 SELECT *
-FROM DSP_MO_HISTORY
-ORDER BY RECEIVED_TIME DESC;
+FROM dsp_mo_history
+ORDER BY received_time DESC;
 
-INSERT INTO DSP_MO_QUEUE
+INSERT INTO dsp_mo_queue
 VALUES (1, '918861983', '', 'KT IP', SYSDATE, '9034', '3');
 COMMIT;
 
@@ -509,13 +512,13 @@ WHERE isdn = '918861983';
 SELECT *
 FROM dip_package;
 SELECT *
-FROM DSP_MT_QUEUE
-ORDER BY PROCESS_TIME DESC;
+FROM dsp_mt_queue
+ORDER BY process_time DESC;
 
 SELECT *
-FROM DIG_SUB_SERVICE;
+FROM dig_sub_service;
 
-INSERT INTO DSP_OWNER.DSP_MO_QUEUE
+INSERT INTO dsp_owner.dsp_mo_queue
 VALUES (112, '918861983', NULL, 'EDU M01667289556902', SYSDATE, '9034', 3);
 COMMIT;
 
@@ -527,40 +530,57 @@ WHERE a.transaction_id = b.transaction_id
 
 
 SELECT *
-FROM DSP_TRANSACTION
-WHERE TRANSACTION_ID IN (SELECT TRANSACTION_ID FROM DSP_ORDER_TRANSACTION GROUP BY TRANSACTION_ID HAVING COUNT(1) > 1)
-  AND RES_ORDER_ID IS NOT NULL;
+FROM dsp_transaction
+WHERE transaction_id IN (SELECT transaction_id FROM dsp_order_transaction GROUP BY transaction_id HAVING COUNT(1) > 1)
+  AND res_order_id IS NOT NULL;
 
 SELECT *
-FROM DSP_ORDER_TRANSACTION
-WHERE TRANSACTION_ID = 1293;
+FROM dsp_order_transaction
+WHERE transaction_id = 1293;
 
 SELECT *
-FROM DSP_TRANSACTION
-WHERE RES_ORDER_ID = 'ruTsQGKTOcsOhKk6Phg7/er6KSo=';
+FROM dsp_transaction
+WHERE res_order_id = 'ruTsQGKTOcsOhKk6Phg7/er6KSo=';
 
-SELECT * from API_REQUEST ORDER BY REQ_TIME desc;
+SELECT *
+FROM api_request
+ORDER BY req_time DESC;
 
 
-SELECT u.user_id, u.user_name,u.password,u.expire_status,u.modified_password, c.api_public_key
-FROM am_user u, am_group_user ug, am_group g, dsp_company c
-WHERE     u.user_id = ug.user_id
-  AND ug.GROUP_ID = g.GROUP_ID
+SELECT u.user_id, u.user_name, u.password, u.expire_status, u.modified_password, c.api_public_key
+FROM am_user u,
+     am_group_user ug,
+     am_group g,
+     dsp_company c
+WHERE u.user_id = ug.user_id
+  AND ug.group_id = g.group_id
   AND u.user_id = c.api_user_id
   AND c.api_public_key IS NOT NULL
-  AND g.GROUP_ID IN (SELECT par_value FROM ap_param
-                     WHERE par_name = 'API_GROUP_ID' AND par_type = 'SYSTEM')
-  AND NVL (g.status, 1) > 0
-  AND NVL (u.status, 1) > 0 order by u.USER_NAME;
+  AND g.group_id IN (SELECT par_value
+                     FROM ap_param
+                     WHERE par_name = 'API_GROUP_ID'
+                       AND par_type = 'SYSTEM')
+  AND NVL(g.status, 1) > 0
+  AND NVL(u.status, 1) > 0
+ORDER BY u.user_name;
 
-select * from DSP_COMPANY where COM_ID=621;
-SELECT * from DIP_REQUEST_HIST ORDER BY REQUEST_TIME desc;
+SELECT *
+FROM dsp_company
+WHERE com_id = 621;
+SELECT *
+FROM dip_request_hist
+ORDER BY request_time DESC;
 
-select * from DSP_DD_HISTORY;
+SELECT *
+FROM dsp_dd_history;
 
-select * from DSP_SMS_COMMAND where CMD_CODE='CMD_DK_DIP_OK';
-alter TABLE  DIP_REQUEST_HIST ADD ORDER_ID       NUMBER(15) ;
-select * from dip_sub_service;
+SELECT *
+FROM dsp_sms_command
+WHERE cmd_code = 'CMD_DK_DIP_OK';
+ALTER TABLE dip_request_hist
+    ADD order_id NUMBER(15);
+SELECT *
+FROM dip_sub_service;
 
 
 SELECT MAX((100 - ROUND((NVL(b.bytes_free, 0) / a.bytes_alloc) * 100, 2)))
@@ -577,7 +597,48 @@ FROM (SELECT f.tablespace_name,
 WHERE a.tablespace_name = b.tablespace_name(+)
   AND a.tablespace_name = 'DATA';
 
-SELECT * from  API_REQUEST order by REQ_ID desc;
+SELECT *
+FROM api_request
+WHERE req_time > TRUNC(SYSDATE)
+ORDER BY req_id DESC;
+
+SELECT *
+FROM dsp_white_list
+WHERE isdn = '896190107';
+
+SELECT *
+FROM dsp_sys_log
+WHERE exec_datetime > TRUNC(SYSDATE)
+  AND trans_id = 'vmedia_api_com_1670224040163';
+
+--Ds cty khu vuc
+WITH user_type AS (SELECT type, user_id, com_id
+                   FROM dsp_company_leveled
+                   WHERE user_id = ?
+                      OR group_id IN (SELECT group_id FROM am_group_user WHERE user_id = ?))
+SELECT c.com_id, c.com_name, c.user_id
+FROM dsp_company_leveled c,
+     user_type
+WHERE c.type IN (1)
+  AND (user_type.type = 0
+    OR (user_type.type = 1 AND c.com_id = user_type.com_id));
 
 
-select * from DSP_SYS_LOG where EXEC_DATETIME>trunc(sysdate) and TRANS_ID ='vmedia_api_com_1670224040163';
+SELECT *
+FROM dsp_service;
+
+--Dong bo bang cuoc Rating Table
+SELECT com_id, tab_id
+FROM dsp_com_price
+WHERE tab_id IN (SELECT tab_id FROM dsp_service_price_tab WHERE service_id = 77 AND status = 1)
+  AND status = 1;
+
+
+SELECT *
+FROM dsp_service_price_tab WHERE tab_id=641;
+
+SELECT *
+FROM dsp_service_price where tab_id=641;
+
+SELECT *
+FROM dsp_service_price_tab;
