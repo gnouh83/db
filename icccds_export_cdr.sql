460142d9a737eda6f68cab2cd6654bb4aba1dac9
HuongNV <huongnv@telsoft.com.vn>
2024-10-08 09:24:01 +0700

    - Updated add partition, rename partition script

--- icccds_export_cdr.sql
+++ icccds_export_cdr.sql
@@ -37,58 +37,81 @@ CREATE TABLE voucher_order
 )
     TABLESPACE "DATA" PARTITION BY RANGE (exp_date)
 (
-    PARTITION "DATA-20240512" VALUES LESS THAN (TO_DATE(' 2024-05-13 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240513" VALUES LESS THAN (TO_DATE(' 2024-05-14 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240514" VALUES LESS THAN (TO_DATE(' 2024-05-15 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240515" VALUES LESS THAN (TO_DATE(' 2024-05-16 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240516" VALUES LESS THAN (TO_DATE(' 2024-05-17 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240517" VALUES LESS THAN (TO_DATE(' 2024-05-18 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240518" VALUES LESS THAN (TO_DATE(' 2024-05-19 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240519" VALUES LESS THAN (TO_DATE(' 2024-05-20 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240520" VALUES LESS THAN (TO_DATE(' 2024-05-21 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240521" VALUES LESS THAN (TO_DATE(' 2024-05-22 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240522" VALUES LESS THAN (TO_DATE(' 2024-05-23 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240523" VALUES LESS THAN (TO_DATE(' 2024-05-24 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240524" VALUES LESS THAN (TO_DATE(' 2024-05-25 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240525" VALUES LESS THAN (TO_DATE(' 2024-05-26 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240526" VALUES LESS THAN (TO_DATE(' 2024-05-27 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240527" VALUES LESS THAN (TO_DATE(' 2024-05-28 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240528" VALUES LESS THAN (TO_DATE(' 2024-05-29 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240529" VALUES LESS THAN (TO_DATE(' 2024-05-30 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240530" VALUES LESS THAN (TO_DATE(' 2024-05-31 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN')),
-    PARTITION "DATA-20240531" VALUES LESS THAN (TO_DATE(' 2024-06-01 00:00:00', 'SYYYY-MM-DD HH24:MI:SS', 'NLS_CALENDAR=GREGORIAN'))
+    PARTITION "DATA-20240512" VALUES LESS THAN (TO_DATE(' 2024-05-13 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240513" VALUES LESS THAN (TO_DATE(' 2024-05-14 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240514" VALUES LESS THAN (TO_DATE(' 2024-05-15 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240515" VALUES LESS THAN (TO_DATE(' 2024-05-16 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240516" VALUES LESS THAN (TO_DATE(' 2024-05-17 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240517" VALUES LESS THAN (TO_DATE(' 2024-05-18 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240518" VALUES LESS THAN (TO_DATE(' 2024-05-19 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240519" VALUES LESS THAN (TO_DATE(' 2024-05-20 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240520" VALUES LESS THAN (TO_DATE(' 2024-05-21 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240521" VALUES LESS THAN (TO_DATE(' 2024-05-22 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240522" VALUES LESS THAN (TO_DATE(' 2024-05-23 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240523" VALUES LESS THAN (TO_DATE(' 2024-05-24 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240524" VALUES LESS THAN (TO_DATE(' 2024-05-25 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240525" VALUES LESS THAN (TO_DATE(' 2024-05-26 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240526" VALUES LESS THAN (TO_DATE(' 2024-05-27 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240527" VALUES LESS THAN (TO_DATE(' 2024-05-28 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240528" VALUES LESS THAN (TO_DATE(' 2024-05-29 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240529" VALUES LESS THAN (TO_DATE(' 2024-05-30 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240530" VALUES LESS THAN (TO_DATE(' 2024-05-31 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN')),
+    PARTITION "DATA-20240531" VALUES LESS THAN (TO_DATE(' 2024-06-01 00:00:00', 'SYYYY-MM-DD HH24:MI:SS',
+                                                        'NLS_CALENDAR=GREGORIAN'))
 );
 
 
 CREATE TABLE "DR_FILE"
-   (	"FILE_ID" NUMBER(10,0),
-	"FILE_NAME" VARCHAR2(200 BYTE),
-	"FILE_MODIFIED" DATE,
-	"FILE_SIZE" NUMBER(10,0),
-	"FILE_TYPE" VARCHAR2(10 BYTE),
-	"TOTAL_RECORD" NUMBER(10,0),
-	"PROCESSED_RECORD" NUMBER(10,0),
-	"PROCESSED_TIME" DATE,
-	"DATA_BEGIN_TIME" DATE,
-	"DATA_END_TIME" DATE,
-	"STATUS" NUMBER(2,0)
-   ) ;
+(
+    "FILE_ID"          NUMBER(10, 0),
+    "FILE_NAME"        VARCHAR2(200 BYTE),
+    "FILE_MODIFIED"    DATE,
+    "FILE_SIZE"        NUMBER(10, 0),
+    "FILE_TYPE"        VARCHAR2(10 BYTE),
+    "TOTAL_RECORD"     NUMBER(10, 0),
+    "PROCESSED_RECORD" NUMBER(10, 0),
+    "PROCESSED_TIME"   DATE,
+    "DATA_BEGIN_TIME"  DATE,
+    "DATA_END_TIME"    DATE,
+    "STATUS"           NUMBER(2, 0)
+);
 --------------------------------------------------------
 --  DDL for Index DR_FILE_PK
 --------------------------------------------------------
 
-  CREATE UNIQUE INDEX "DR_FILE_PK" ON "DR_FILE" ("FILE_ID");
+CREATE UNIQUE INDEX "DR_FILE_PK" ON "DR_FILE" ("FILE_ID");
 --------------------------------------------------------
 --  Constraints for Table DR_FILE
 --------------------------------------------------------
 
-  ALTER TABLE "DR_FILE" ADD CONSTRAINT "DR_FILE_PK" PRIMARY KEY ("FILE_ID")
-  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS
-  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
-  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
-  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
-  TABLESPACE "DATA"  ENABLE;
-  ALTER TABLE "DR_FILE" MODIFY ("FILE_ID" NOT NULL ENABLE);
+ALTER TABLE "DR_FILE"
+    ADD CONSTRAINT "DR_FILE_PK" PRIMARY KEY ("FILE_ID")
+        USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS
+            STORAGE (INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+            PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+            BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+            TABLESPACE "DATA" ENABLE;
+ALTER TABLE "DR_FILE"
+    MODIFY ("FILE_ID" NOT NULL ENABLE);
 --3. Ket xuat du lieu
 SELECT SUBSTR(a.description, INSTR(a.description, '->', -1, 1) + 2) isdn,
        c.profile_code                                               ma_goi,
@@ -114,16 +137,17 @@ WHERE a.transaction_id = b.transaction_id
   AND a.request_time >= TRUNC(SYSDATE - 1)
   AND a.request_time < TRUNC(SYSDATE)
 UNION ALL
-SELECT SUBSTR(f.ref, INSTR(f.ref, '|', -1, 1) + 1) isdn,
-       c.profile_code                              ma_goi,
-       c.price                                     gia_tien,
-       a.transaction_id                            ma_dh,
-       d.total_cost                                gtdh,
-       (1 - d.total_cost / d.contract_cost) * 100  ck,
-       e.com_id                                    ma_dly,
-       e.com_name                                  ten_dly,
-       TO_CHAR(f.use_tim, 'dd/mm/yyyy hh24:mi:ss') thoigian_dk,
-       'DATACODE'                                  type
+SELECT NVL(substr(f.ref, instr(f.ref, '|', -1, 1) + 1),
+           SUBSTR(f.ref, INSTR(f.ref, '|') + 1, INSTR(f.ref, '|', 1, 2) - INSTR(f.ref, '|') - 1)) isdn,
+       c.profile_code                                                                             ma_goi,
+       c.price                                                                                    gia_tien,
+       a.transaction_id                                                                           ma_dh,
+       d.total_cost                                                                               gtdh,
+       (1 - d.total_cost / d.contract_cost) * 100                                                 ck,
+       e.com_id                                                                                   ma_dly,
+       e.com_name                                                                                 ten_dly,
+       TO_CHAR(f.use_tim, 'dd/mm/yyyy hh24:mi:ss')                                                thoigian_dk,
+       'DATACODE'                                                                                 type
 FROM m_transaction a,
      m_transaction_detail b,
      profile c,
--- util-script.sql
+++ util-script.sql
@@ -20,7 +20,7 @@ FROM dba_part_tables t
          t.table_name = s.segment_name
              AND t.owner = s.owner
 WHERE s.segment_type = 'TABLE PARTITION'
-  AND t.owner = 'SCHEMA_NAME'
+  AND t.owner = 'DSP_OWNER'
 GROUP BY t.owner,
          t.table_name
 ORDER BY size_mb DESC;
@@ -31,51 +31,68 @@ FROM dba_data_files;
 
 SELECT ROUND(SUM(bytes) / (1024 * 1024 * 1024), 2) AS used_size_gb
 FROM dba_segments;
-
+--Dung lượng archivelog theo từng ngày
+SELECT TO_CHAR(first_time, 'YYYY-MM-DD') AS archive_date,
+       SUM(blocks * block_size)          AS total_size
+FROM v$archived_log
+GROUP BY TO_CHAR(first_time, 'YYYY-MM-DD')
+ORDER BY archive_date;
 -- Add partition
 DECLARE
-    v_start_date DATE := TO_DATE('01-JUN-2024', 'DD-MON-YYYY');
-    v_end_date DATE := TO_DATE('30-JUN-2029', 'DD-MON-YYYY');
-    v_partition_name VARCHAR2(50);
+    v_start_date      DATE := TO_DATE('01-JUN-2024', 'DD-MON-YYYY');
+    v_end_date        DATE := TO_DATE('30-JUN-2029', 'DD-MON-YYYY');
+    v_partition_name  VARCHAR2(50);
     v_partition_limit DATE;
-    v_sql VARCHAR2(1000);
+    v_sql             VARCHAR2(1000);
 BEGIN
-    WHILE v_start_date <= v_end_date LOOP
-        v_partition_name := 'DATA_' || TO_CHAR(v_start_date, 'YYYYMMDD');
-        v_partition_limit := v_start_date + 1;
+    WHILE v_start_date <= v_end_date
+        LOOP
+            v_partition_name := 'DATA_' || TO_CHAR(v_start_date, 'YYYYMMDD');
+            v_partition_limit := v_start_date + 1;
 
-        v_sql := 'ALTER TABLE voucher_order ADD PARTITION ' || v_partition_name ||
-                 ' VALUES LESS THAN (TO_DATE(''' || TO_CHAR(v_partition_limit, 'DD-MON-YYYY') || ''', ''DD-MON-YYYY'')) TABLESPACE DATA';
+            v_sql := 'ALTER TABLE voucher_order ADD PARTITION ' || v_partition_name ||
+                     ' VALUES LESS THAN (TO_DATE(''' || TO_CHAR(v_partition_limit, 'DD-MON-YYYY') ||
+                     ''', ''DD-MON-YYYY'')) TABLESPACE DATA';
 
-        EXECUTE IMMEDIATE v_sql;
+            EXECUTE IMMEDIATE v_sql;
 
-        --DBMS_OUTPUT.PUT_LINE(v_sql);
-        v_start_date := v_start_date + 1;
-    END LOOP;
+            --DBMS_OUTPUT.PUT_LINE(v_sql);
+            v_start_date := v_start_date + 1;
+        END LOOP;
 END;
 
 --Rename partition
 DECLARE
-    v_start_date DATE := TO_DATE('01-MAY-2024', 'DD-MON-YYYY');
-    v_end_date DATE := TO_DATE('31-MAY-2024', 'DD-MON-YYYY');
-    v_partition_name VARCHAR2(50);
+    v_start_date      DATE := TO_DATE('01-MAY-2024', 'DD-MON-YYYY');
+    v_end_date        DATE := TO_DATE('31-MAY-2024', 'DD-MON-YYYY');
+    v_partition_name  VARCHAR2(50);
     v_partition_limit DATE;
-    v_sql VARCHAR2(1000);
+    v_sql             VARCHAR2(1000);
 BEGIN
     -- Đổi tên partition hiện có (ví dụ)
     DBMS_OUTPUT.PUT_LINE('ALTER TABLE voucher_order RENAME PARTITION p_existing TO p_20240430;');
 
     -- Thêm các partition mới từ 1/5/2024 đến 30/6/2024
-    WHILE v_start_date <= v_end_date LOOP
-        v_partition_name := 'p_' || TO_CHAR(v_start_date, 'YYYYMMDD');
-        v_partition_limit := v_start_date + 1;
+    WHILE v_start_date <= v_end_date
+        LOOP
+            v_partition_name := 'p_' || TO_CHAR(v_start_date, 'YYYYMMDD');
+            v_partition_limit := v_start_date + 1;
 
-        v_sql := 'ALTER TABLE voucher_order ADD PARTITION ' || v_partition_name ||
-                 ' VALUES LESS THAN (TO_DATE(''' || TO_CHAR(v_partition_limit, 'DD-MON-YYYY') || ''', ''DD-MON-YYYY'')) TABLESPACE DATA;';
+            v_sql := 'ALTER TABLE voucher_order ADD PARTITION ' || v_partition_name ||
+                     ' VALUES LESS THAN (TO_DATE(''' || TO_CHAR(v_partition_limit, 'DD-MON-YYYY') ||
+                     ''', ''DD-MON-YYYY'')) TABLESPACE DATA;';
 
-        DBMS_OUTPUT.PUT_LINE(v_sql);
+            DBMS_OUTPUT.PUT_LINE(v_sql);
 
-        v_start_date := v_start_date + 1;
-    END LOOP;
+            v_start_date := v_start_date + 1;
+        END LOOP;
 END;
 /
+
+
+
+select NVL(substr(f.ref, instr(f.ref, '|', -1, 1) + 1),
+           SUBSTR(f.ref, INSTR(f.ref, '|') + 1, INSTR(f.ref, '|', 1, 2) - INSTR(f.ref, '|') - 1)) isdn,
+       f.*
+from voucher_order partition (data_20240612) f
+where profile_code = 'DH2';
